c451f35602878efda11f0ba4ddb497da15681072
Swastik Paratkar <130985359+SwastikParatkar@users.noreply.github.com>
2026-06-10 18:30:18 +0530

    Refactor main function for command handling
    Refactor main function to remove CSV reading and GOB encoding.

--- cmd/gobtags/main.go
+++ cmd/gobtags/main.go
@@ -1,45 +1,52 @@
 package main
 
 import (
-	"encoding/csv"
-	"encoding/gob"
-	"fmt"
-	"os"
+	"log"
+	"net"
+	"strconv"
+
+	"github.com/iomz/golemu/config"
+	"github.com/iomz/golemu/connection"
+	"github.com/iomz/golemu/server"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-type Tag struct {
-	EPC string
-}
-
 func main() {
+	cmd := kingpin.MustParse(config.App.Parse(os.Args[1:]))
 
-	file, err := os.Open("tags.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-
-	rows, err := reader.ReadAll()
-	if err != nil {
-		panic(err)
+	if *config.Debug {
+		log.SetLevel(log.DebugLevel)
 	}
 
-	var tags []Tag
-
-	for _, row := range rows {
-		tags = append(tags, Tag{EPC: row[1]})
+	var exitCode int
+	switch cmd {
+	case config.Client.FullCommand():
+		client := connection.NewClient(config.IP.String(), *config.Port)
+		exitCode = client.Run()
+	case config.Server.FullCommand():
+		srv := server.NewServer(
+			config.IP.String(),
+			*config.Port,
+			*config.APIPort,
+			*config.PDU,
+			*config.ReportInterval,
+			*config.KeepaliveInterval,
+			*config.InitialMessageID,
+			*config.File,
+		)
+		exitCode = srv.Run()
+	case config.Simulator.FullCommand():
+		sim := connection.NewSimulator(
+			config.IP.String(),
+			*config.Port,
+			*config.PDU,
+			*config.ReportInterval,
+			*config.SimulationDir,
+			*config.InitialMessageID,
+		)
+		exitCode = sim.Run()
 	}
 
-	out, err := os.Create("tags.gob")
-	if err != nil {
-		panic(err)
-	}
-	defer out.Close()
-
-	encoder := gob.NewEncoder(out)
-	encoder.Encode(tags)
-
-	fmt.Println("tags.gob created successfully")
+	os.Exit(exitCode)
 }
